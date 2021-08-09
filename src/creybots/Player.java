2773e5f37a17679d63d9c2e73417596f2de6f89c
julian001-cmd <3214218927ju@gmail.com>
2021-08-09 13:14:05 -0500

    update

---
+++ src/creybots/Player.java
@@ -0,0 +1,169 @@
+package creybots;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+public class Player {
+
+
+    public static int MASK_DUDE = 0;
+    public static int NINJA_FROG = 1;
+    public static int PINK_MAN = 2;
+    public static int VIRTUAL_GUY = 3;
+
+    public static int STATE_IDLE = 0;
+    public static int STATE_RUN = 1;
+    public static int STATE_JUMP = 2;
+    public static int STATE_FALL = 3;
+
+    private Point position;
+    private int type;
+    private int state = STATE_IDLE;
+
+    private int width = 32;
+    private int height = 32;
+
+    public Player(int type,Point initialPoint) {
+        this.type = type;
+        this.position = initialPoint;
+
+        loadImage();
+
+    }
+
+    private BufferedImage imageIdle;
+    private BufferedImage imageRun;
+    private BufferedImage imageJump;
+    private BufferedImage imageFall;
+    private BufferedImage imageHit;
+    
+    private void loadImage() {
+        String[] pathImages = {
+            "image/characters/Ninja Frog/",
+            "image/characters/Mask Dude/",
+            "image/characters/Pink Man/",
+            "image/characters/Virtual Guy/"
+        };      
+        
+        String pathIdle = pathImages[type]+"Idle.png";
+        String pathRun = pathImages[type]+"Run.png";
+        String pathJump = pathImages[type]+"Jump (32x32).png";
+        String pathFall = pathImages[type]+"Fall (32x32).png";
+        String pathHit = pathImages[type]+"Hit (32x32).png";         
+
+        try {
+
+            imageIdle = ImageIO.read(new File(pathIdle));
+            imageRun = ImageIO.read(new File(pathRun));
+            imageJump = ImageIO.read(new File(pathJump));
+            imageFall = ImageIO.read(new File(pathFall));
+            imageHit = ImageIO.read(new File(pathHit));
+            
+        } catch (Exception e) {
+            System.err.println("No se pudieron cargar imagenes");
+            System.err.println(e.getMessage());
+            System.exit(0);
+        }
+
+    }
+
+    private int imageReference = 0;
+    private int jumpDistance = height;
+
+    public void updatePlayer() {
+        if(state == STATE_IDLE) {
+
+            if(imageReference < (imageIdle.getWidth()/width)-1) {
+                imageReference++;
+            } else {
+                imageReference = 0;
+            }
+
+
+        } else if(state == STATE_JUMP) {
+
+            if(position.y > (baseLine-jumpDistance)) {
+                position.y = position.y - 2;
+            } else {
+                fall();
+            }
+
+        } else if(state == STATE_FALL) {
+
+            if(position.y < baseLine) {
+                position.y = position.y + 2;
+            } else {
+                state = previousState;
+            }
+
+        } else if(state == STATE_RUN) {
+            if(imageReference < (imageRun.getWidth()/width)-1) {
+                imageReference++;
+            } else {
+                imageReference = 0;
+            }
+        
+        }
+    }
+
+    public BufferedImage getImagePlayer() {
+        int x = imageReference*width;
+        if(state == STATE_RUN) {
+            return imageRun.getSubimage(x,0,width,height);
+        } else if(state == STATE_JUMP) {   
+            return imageJump;
+        } else if(state == STATE_FALL) {   
+            return imageFall;            
+        } else {
+            return imageIdle.getSubimage(x,0,width,height);
+        }
+    }
+
+
+    public int getX() {
+        return position.x;
+    }
+
+    public int getY() {
+        return position.y;
+    }
+
+
+    public void idle() {
+        if(state == Player.STATE_RUN) {
+            state = STATE_IDLE;
+        }
+    }
+
+    public void run() {
+        if(state == Player.STATE_IDLE) {
+            state = STATE_RUN;
+        }
+    }
+
+
+    private int previousState = 0;
+    private int baseLine;
+
+    public void jump() {
+        if((state == Player.STATE_RUN) || (state == Player.STATE_IDLE)) {
+            previousState = state;
+            state = Player.STATE_JUMP;
+            baseLine = position.y;
+        }
+    }
+
+    public void fall() {
+        state = Player.STATE_FALL;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+}
---
+++ src/creybots/RunnerOne.java
@@ -0,0 +1,136 @@
+package creybots;
+
+
+import javax.swing.JFrame;
+import javax.imageio.ImageIO;
+import java.awt.image.*;
+import java.awt.*;
+import java.io.File;
+import javax.swing.Timer;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+
+public class RunnerOne extends JFrame implements ActionListener {
+
+    public static int FRAME_WIDTH = 640;
+    public static int FRAME_HEIGHT = 480;
+
+    public final int DELAY = 50;
+
+    private Background background;
+
+    private Player player;
+
+    private Timer timer;
+
+    private boolean pause = false;
+
+    public RunnerOne() {
+        setTitle("RunnerOne");
+
+        this.setSize(FRAME_WIDTH,FRAME_HEIGHT);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setLocation(dim.width/2-this.getSize().width,dim.height/2-this.getSize().height/2);
+
+        addKeyListener(new GameKeys());
+
+        /*
+         * Background
+         */
+        background = new Background("image/bgd1.png");
+
+
+        /*
+         * Player
+         */
+        player = new Player(Player.MASK_DUDE,new Point(50,410));
+
+        //Timer
+        timer = new Timer(DELAY, this);
+        timer.start();
+
+        setVisible(true);
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawImage(background.getImageBackground(),0,0,null);
+
+        /*
+         * Player
+         */
+        g.drawImage(player.getImagePlayer(),player.getX(),player.getY(),null);
+    }
+
+    public void updateGame() {
+        if(pause) {
+            return;
+        }
+
+        if(player.getState() != Player.STATE_IDLE) {
+            background.updateBackground();
+        }
+
+        /*
+         * Player
+         */
+        player.updatePlayer();
+    }
+
+    public static void main(String[] args) throws Exception {
+        RunnerOne runnerOne = new RunnerOne();
+    }
+
+    @Override
+    public void actionPerformed(java.awt.event.ActionEvent e) {
+
+        
+        updateGame();
+
+        repaint();
+
+    }
+
+
+    public class GameKeys extends KeyAdapter {
+		public void keyPressed(KeyEvent e) {
+            
+			if(e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                System.exit(0);
+            } else if(e.getKeyCode() ==  KeyEvent.VK_P) {
+                pause = !pause;
+
+            /*
+             * RIGHT
+             */
+            } else if(e.getKeyCode() ==  KeyEvent.VK_RIGHT) {
+                player.run();
+            } else if(e.getKeyCode() ==  KeyEvent.VK_D) {
+                player.run();
+            
+
+            /*
+             * JUMP
+             */
+            } else if(e.getKeyCode() ==  KeyEvent.VK_UP) {
+                player.jump();
+            }
+            
+        }
+
+
+        public void keyReleased(KeyEvent e) {
+            if(e.getKeyCode() ==  KeyEvent.VK_RIGHT) {
+                player.idle();
+            } else if(e.getKeyCode() ==  KeyEvent.VK_D) {
+                player.run();                
+            }
+        }
+    }
+}
